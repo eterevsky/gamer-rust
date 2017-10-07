ecfb850cffcd98f48e981038c04dee86181e60f8
Oleg Eterevsky <oleg@eterevsky.com>
2017-10-07 15:11:13 +0200

    Move benchmarks to a separate module.

---
+++ src/benchmarks.rs
@@ -0,0 +1,82 @@
+use rand;
+use time;
+
+use def::{Agent, Game, State};
+use gomoku::{Gomoku, GomokuLinesEvaluator};
+use minimax::MiniMaxAgent;
+use subtractor::Subtractor;
+use terminal_evaluator::TerminalEvaluator;
+
+fn bench_random_game<'g, G: Game<'g>>(game: &'g G) -> f64 {
+  const N: u32 = 1_000_000;
+
+  let start = time::precise_time_s();
+  let mut rng = rand::XorShiftRng::new_unseeded();
+
+  for _ in 0..N {
+    let mut state: G::State = game.new_game();
+    while let Some(m) = state.get_random_move(&mut rng) {
+      state.play(m).unwrap();
+    }
+    state.get_payoff().unwrap();
+  }
+
+  let end = time::precise_time_s();
+  ((end - start) as f64) / (N as f64)
+}
+
+fn bench_random_gomoku() {
+  let game = Gomoku::new();
+  let time = bench_random_game(&game);
+  println!("bench_random_gomoku: {} us\n", 1E6 * time);
+}
+
+fn bench_random_subtractor100() {
+  let game = Subtractor::new(100, 4);
+  let time = bench_random_game(&game);
+  println!("bench_random_subtractor100: {} us\n", 1E6 * time);
+}
+
+fn bench_minimax_subtractor() {
+  let game = Subtractor::new(21, 4);
+  let state = game.new_game();
+  let mut agent = MiniMaxAgent::new(TerminalEvaluator::new(), 20, 1.0);
+  let report = agent.select_move(&state).unwrap();
+
+  println!("bench_minimax_subtractor:\n{}", report);
+}
+
+fn bench_minimax_gomoku_start() {
+  let game = Gomoku::new();
+  let state = game.new_game();
+  let mut agent = MiniMaxAgent::new(
+      GomokuLinesEvaluator::new_default(), 3, 1000.0);
+  let report = agent.select_move(&state).unwrap();
+
+  println!("bench_minimax_gomoku_start:\n{}", report);
+}
+
+fn bench_minimax_gomoku_forced() {
+  let game = Gomoku::new();
+  let mut state = game.new_game();
+  state.play("A1".parse().unwrap()).unwrap();
+  state.play("B1".parse().unwrap()).unwrap();
+  state.play("A2".parse().unwrap()).unwrap();
+  state.play("B2".parse().unwrap()).unwrap();
+  state.play("A3".parse().unwrap()).unwrap();
+  state.play("B3".parse().unwrap()).unwrap();
+  state.play("A4".parse().unwrap()).unwrap();
+  let mut agent = MiniMaxAgent::new(GomokuLinesEvaluator::new_default(), 3, 1000.0);
+  let report = agent.select_move(&state).unwrap();
+
+  println!("bench_minimax_gomoku_forced:\n{}", report);
+}
+
+pub fn run() {
+  bench_random_gomoku();
+  bench_random_subtractor100();
+  bench_minimax_subtractor();
+  bench_minimax_gomoku_start();
+  bench_minimax_gomoku_forced();
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,16 @@
-
 extern crate clap;
 extern crate rand;
 extern crate time;
 
 use clap::{App, Arg, SubCommand};
-use std::iter;
 
 extern crate gamer;
 
-use gamer::def::{Agent, AgentReport, Evaluator, Game, State};
+use gamer::benchmarks;
+use gamer::def::{Agent, AgentReport, Game, State};
 use gamer::gomoku::{Gomoku, GomokuLinesEvaluator, GomokuState, GomokuLineFeatureExtractor};
 use gamer::feature_evaluator::{FeatureEvaluator, LinearRegression, Regression};
 use gamer::minimax::MiniMaxAgent;
-use gamer::subtractor::{Subtractor, SubtractorFeatureExtractor};
-use gamer::terminal_evaluator::TerminalEvaluator;
-
-fn bench_random_game<'g, G: Game<'g>>(game: &'g G) -> f64 {
-  const N: u32 = 1_000_000;
-
-  let start = time::precise_time_s();
-  let mut rng = rand::XorShiftRng::new_unseeded();
-
-  for _ in 0..N {
-    let mut state: G::State = game.new_game();
-    while let Some(m) = state.get_random_move(&mut rng) {
-      state.play(m).unwrap();
-    }
-    state.get_payoff().unwrap();
-  }
-
-  let end = time::precise_time_s();
-  ((end - start) as f64) / (N as f64)
-}
-
-fn bench_random_gomoku() {
-  let game = Gomoku::new();
-  let time = bench_random_game(&game);
-  println!("bench_random_gomoku: {} us\n", 1E6 * time);
-}
-
-fn bench_random_subtractor100() {
-  let game = Subtractor::new(100, 4);
-  let time = bench_random_game(&game);
-  println!("bench_random_subtractor100: {} us\n", 1E6 * time);
-}
-
-fn bench_minimax_subtractor() {
-  let game = Subtractor::new(21, 4);
-  let state = game.new_game();
-  let mut agent = MiniMaxAgent::new(TerminalEvaluator::new(), 20, 1.0);
-  let report = agent.select_move(&state).unwrap();
-
-  println!("bench_minimax_subtractor:\n{}", report);
-}
-
-fn bench_minimax_gomoku_start() {
-  let game = Gomoku::new();
-  let state = game.new_game();
-  let mut agent = MiniMaxAgent::new(
-      GomokuLinesEvaluator::new_default(), 3, 1000.0);
-  let report = agent.select_move(&state).unwrap();
-
-  println!("bench_minimax_gomoku_start:\n{}", report);
-}
-
-fn bench_minimax_gomoku_forced() {
-  let game = Gomoku::new();
-  let mut state = game.new_game();
-  state.play("A1".parse().unwrap()).unwrap();
-  state.play("B1".parse().unwrap()).unwrap();
-  state.play("A2".parse().unwrap()).unwrap();
-  state.play("B2".parse().unwrap()).unwrap();
-  state.play("A3".parse().unwrap()).unwrap();
-  state.play("B3".parse().unwrap()).unwrap();
-  state.play("A4".parse().unwrap()).unwrap();
-  let mut agent = MiniMaxAgent::new(GomokuLinesEvaluator::new_default(), 3, 1000.0);
-  let report = agent.select_move(&state).unwrap();
-
-  println!("bench_minimax_gomoku_forced:\n{}", report);
-}
-
-fn bench() {
-  bench_random_gomoku();
-  bench_random_subtractor100();
-  bench_minimax_subtractor();
-  bench_minimax_gomoku_start();
-  bench_minimax_gomoku_forced();
-}
 
 fn play_gomoku() {
   let game = Gomoku::new();
@@ -169,7 +93,7 @@ fn main() {
   let args = args_definition().get_matches();
 
   if args.subcommand_matches("bench").is_some() {
-    bench();
+    benchmarks::run();
   } else if args.subcommand_matches("play").is_some() {
     play_gomoku();
   } else if args.subcommand_matches("train").is_some() {
