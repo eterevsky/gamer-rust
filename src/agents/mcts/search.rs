ddebbcbbb69c9bdadc4db7724777aac3e19de2cd
Oleg Eterevsky <oleg@eterevsky.com>
2018-05-25 21:31:00 +0200

    Start MCT search.

--- src/agents/mcts/search.rs
+++ src/agents/mcts/search.rs
@@ -1,7 +1,7 @@
 use rand;
 use std::time::Instant;
 
-use def::{State, Policy, Evaluator};
+use def::{Evaluator, Policy, State};
 use super::report::MctsReport;
 
 pub struct MctsSearch<S: State, P: Policy<S>, E: Evaluator<S>> {
@@ -15,15 +15,38 @@ impl<S: State, P: Policy<S>, E: Evaluator<S>> MctsSearch<S, P, E> {
     MctsSearch {
       policy,
       evaluator,
-      root_state: state
+      root_state: state,
     }
   }
 
-  pub fn search(&self, max_samples: u64, deadline: Instant) -> Result<MctsReport<S::Move>, &'static str> {
+  pub fn search(
+    &self,
+    max_samples: u32,
+    deadline: Instant,
+  ) -> Result<MctsReport<S::Move>, &'static str> {
     if self.root_state.is_terminal() {
       return Err("the state is terminal");
     }
 
-    Ok(MctsReport::new(self.root_state.get_random_move(&mut rand::weak_rng()).unwrap(), 0, 0., self.root_state.player()))
+    Ok(MctsReport::new(
+      self
+        .root_state
+        .get_random_move(&mut rand::weak_rng())
+        .unwrap(),
+      0,
+      0.,
+      self.root_state.player(),
+    ))
   }
+}
+
+struct Node<S: State> {
+  samples: u32,
+  score: f32,
+  children: Vec<Node>
+}
+
+impl<S: State> Node<S> {
+  fn 
+
 }
